113345ea611a2183b399e361f9fae0a590654d9d
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-05-22 16:53:00 +0900

    아이템 서비스 코드 작성

--- src/main/java/com/example/miniProject/repository/ItemRepository.java
+++ src/main/java/com/example/miniProject/repository/ItemRepository.java
@@ -15,7 +15,12 @@ public class ItemRepository {
 
     //아이템 JPA에 저장
     public void save(Item item) {
-        em.persist(item);
+        if(item.getId() == null) {  //만약 등록하려는 아이템의 아이디가 없으면 저장(신규 등록)
+            em.persist(item);
+        } else {
+            em.merge(item); //merge는 업데이트 비슷한것 (업데이트)
+        }
+
     }
 
     //아이템 id로 아이템 조회하기
@@ -25,14 +30,14 @@ public class ItemRepository {
 
     //아이템 전체 조회하기
     public List<Item> findAll() {
-        List<Item> itemList = em.createQuery("select m from Item m", Item.class)
+        List<Item> itemList = em.createQuery("select i from Item i", Item.class)
                 .getResultList();
         return itemList;
     }
 
     //아이템 이름으로 조회하기
     public List<Item> findByName(String itemName) {
-        List<Item> itemList = em.createQuery("select m from Item m where m.name = :name", Item.class)
+        List<Item> itemList = em.createQuery("select i from Item i where i.name = :name", Item.class)
                 .setParameter("name", itemName)
                 .getResultList();
         return itemList;
---
+++ src/main/java/com/example/miniProject/service/ItemService.java
@@ -0,0 +1,36 @@
+package com.example.miniProject.service;
+
+import com.example.miniProject.domain.Item;
+import com.example.miniProject.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    //아이템 저장 서비스
+    public void addItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    //아이템 전체 조회
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    //아이템 단일 조회
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+    
+
+
+}
